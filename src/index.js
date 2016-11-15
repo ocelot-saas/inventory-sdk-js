8f4e3feb68b1e049f829b38ebf82aaceb761e96e
Horia Coman <horia141@gmail.com>
2016-11-15 14:38:13 +0200

    Add the getWebshopInfo method as well as a lot of scaffolding for being able to do this across the system.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-export const FOO = 'BAR';
+import { Promise } from 'es6-promise';
+import 'isomorphic-fetch';
+
+
+export class InventoryService {
+
+    constructor(inventoryServiceDomain) {
+        this._inventoryServiceDomain = inventoryServiceDomain;
+    }
+
+    getWebshopInfo(host) {
+        return new Promise(
+            (resolve, reject) => {
+                const options = {
+                    method: 'GET',
+                    headers: {
+			'Host': host
+		    },
+                    mode: 'cors',
+                    cache: 'no-cache',
+                    redirect: 'error',
+                    referrer: 'client'
+                };
+
+                fetch(`http://${this._inventoryServiceDomain}/webshop`, options)
+                    .then((response) => {
+                       if (response.ok) {
+                           response.json()
+                               .then((json) => {
+                                   resolve(json.webshopInfo);
+                               })
+                               .catch((error) => {
+                                   reject(error);
+                               });
+                       } else {
+			   reject(response.status);
+                       }
+                   })
+                   .catch((error) => {
+                       reject(error);
+                   });
+            }
+        );
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const path = require('path');
+const webpack = require('webpack');
 
 
 module.exports = {
     entry: './src/index.js',
     output: {
 	path: path.resolve(__dirname, 'dist'),
-	publicPath: '/dist',
-	filename: '[name].bundle.js'
+	filename: 'inventory-sdk-js.js',
+	libraryTarget: 'commonjs'
     },
+    target: 'node',
     module: {
 	loaders: [{
-            test: /\.(js|jsx)$/,
+            test: /\.(js)$/,
             include: [path.resolve(__dirname, 'src')],
             loader: 'babel',
             query: {
@@ -18,10 +20,21 @@ module.exports = {
                 presets: ['es2015'],
                 plugins: ['transform-runtime']
             }
-        }]
+        }, {
+	    test: /\.(json)$/,
+	    include: [
+		path.resolve(__dirname, 'src'),
+		path.resolve(__dirname, 'node_modules')
+	    ],
+	    loader: 'json'
+	}]
     },
+    plugins: [
+	new webpack.IgnorePlugin(/vertx/),
+	// new webpack.IgnorePlugin(/\/iconv-loader$/)
+    ],
     resolve: {
-	extensions: ['', '.js', '.jsx'],
+	extensions: ['', '.js', '.json'],
 	root: [
 	    path.resolve(__dirname, 'src')
 	]
