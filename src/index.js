6c6a76237c98a5bbc83d56855135519849e4c2a7
Horia Coman <horia141@gmail.com>
2016-11-15 22:18:30 +0200

    Add the getOrgInfo method to the InventoryService

--- src/index.js
+++ src/index.js
@@ -8,14 +8,49 @@ export class InventoryService {
         this._inventoryServiceDomain = inventoryServiceDomain;
     }
 
+    getOrgFromService(accessToken) {
+        return new Promise(
+            (resolve, reject) => {
+                const options = {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`
+                    },
+                    mode: 'cors',
+                    cache: 'no-cache',
+                    redirect: 'error',
+                    referrer: 'client'
+                };
+
+                fetch(`http://${this._inventoryServiceDomain}/org`, options)
+                    .then((response) => {
+                       if (response.ok) {
+                           response.json()
+                               .then((json) => {
+                                   resolve(json.org);
+                               })
+                               .catch((error) => {
+                                   reject(error);
+                               });
+                       } else {
+                           reject(response.status);
+                       }
+                    })
+                    .catch((error) => {
+                       reject(error);
+                    });
+            }
+        );
+    }
+
     getWebshopInfo(host) {
         return new Promise(
             (resolve, reject) => {
                 const options = {
                     method: 'GET',
                     headers: {
-			'Host': host
-		    },
+                        'Host': host
+                    },
                     mode: 'cors',
                     cache: 'no-cache',
                     redirect: 'error',
@@ -33,7 +68,7 @@ export class InventoryService {
                                    reject(error);
                                });
                        } else {
-			   reject(response.status);
+                           reject(response.status);
                        }
                    })
                    .catch((error) => {
