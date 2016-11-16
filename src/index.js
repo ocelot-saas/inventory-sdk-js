ede0e15ff1510b55edd79cd5473778f2cdff4c98
Horia Coman <horia141@gmail.com>
2016-11-16 11:12:51 +0200

    Add most methods, except for platform ones.

--- src/index.js
+++ src/index.js
@@ -8,72 +8,195 @@ export class InventoryService {
         this._inventoryServiceDomain = inventoryServiceDomain;
     }
 
-    getOrgFromService(accessToken) {
-        return new Promise(
-            (resolve, reject) => {
-                const options = {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': `Bearer ${accessToken}`
-                    },
-                    mode: 'cors',
-                    cache: 'no-cache',
-                    redirect: 'error',
-                    referrer: 'client'
-                };
-
-                fetch(`http://${this._inventoryServiceDomain}/org`, options)
-                    .then((response) => {
-                       if (response.ok) {
-                           response.json()
-                               .then((json) => {
-                                   resolve(json.org);
-                               })
-                               .catch((error) => {
-                                   reject(error);
-                               });
-                       } else {
-                           reject(response.status);
-                       }
-                    })
-                    .catch((error) => {
-                       reject(error);
-                    });
-            }
-        );
+    getOrg(accessToken) {
+        return this._fetch(
+            'GET', `http://${this._inventoryServiceDomain}/org`,
+            {'Authorization': `Bearer ${accessToken}`}, (json) => json.org);
     }
 
+    createOrg(accessToken, name, description, keywords, address, openingHours, imageSet) {
+        const orgCreationRequest = {
+            name: name,
+            description: description,
+            keywords: [],
+            address: address,
+            openingHours: openingHours,
+            imageSet: imageSet
+        };
+        
+        return this._fetchWithData(
+            'POST', `http://${this._inventoryServiceDomain}/org`,
+            {'Authorization': `Bearer ${accessToken}`}, (json) => json.org, orgCreationRequest);
+    }
+
+    getRestaurant(accessToken) {
+        return this._fetch(
+            'GET', `http://${this._inventoryServiceDomain}/org/restaurant`,
+            {'Authorization': `Bearer ${accessToken}`}, (json) => json.restaurant);
+    }
+
+    updateRestaurant(accessToken, name, description, address) {
+        const restaurantUpdateRequest = {
+            name: name,
+            description: description,
+            keywords: [],
+            address: address
+        };
+
+        return this._fetchWithData(
+            'PUT', `http://${this._inventoryServiceDomain}/org/restaurant`,
+            {'Authorization': `Bearer ${accessToken}`}, (json) => json.restaurant, restaurantUpdateRequest);
+    }
+
+    createMenuSection(accessToken, name, description) {
+        const menuSectionCreationRequest = {
+            name: name,
+            description: description
+        };
+
+        return this._fetchWithData(
+            'POST', `http://${this._inventoryServiceDomain}/org/menu/sections`,
+            {'Authorization': `Bearer ${accessToken}`}, (json) => json.menuSections[0], menuSectionCreationRequest);
+    }
+
+    getAllMenuSections(accessToken) {
+        return this._fetch(
+            'GET', `http://${this._inventoryServiceDomain}/org/menu/sections`,
+            {'Authorization': `Bearer ${accessToken}`}, (json) => json.menuSections);
+    }
+
+    getMenuSection(accessToken, sectionId) {
+        return this._fetch(
+            'GET', `http://${this._inventoryServiceDomain}/org/menu/sections/${sectionId}`,
+            {'Authorization': `Bearer ${accessToken}`}, (json) => json.menuSection);
+    }
+
+    updateMenuSection(accessToken, sectionId, name, description) {
+        const menuSectionUpdateRequest = {
+            name: name,
+            description: description
+        };
+
+        return this._fetchWithData(
+            'PUT', `http://${this._inventoryServiceDomain}/org/menu/sections/${sectionId}`,
+            {'Authorization': `Bearer ${accessToken}`}, (json) => json.menuSection, menuSectionUpdateRequest);
+    }
+
+    deleteMenuSection(accessToken, sectionId) {
+        return this._fetch(
+            'DELETE', `http://${this._inventoryServiceDomain}/org/menu/sections/${sectionId}`,
+            {'Authorization': `Bearer ${accessToken}`}, (unused) => { return {}; });
+    }
+
+    createMenuItem(accessToken, sectionId, name, description, keywords, ingredients, imageSet) {
+        const menuItemCreationRequest = {
+            sectionId: sectionId,
+            name: name,
+            description: description,
+            keywords: keywords,
+            ingredients: ingredients,
+            imageSet: imageSet
+        };
+
+        return this._fetchWithData(
+            'POST', `http://${this._inventoryServiceDomain}/org/menu/items`,
+            {'Authorization': `Bearer ${accessToken}`}, (json) => json.menuItems[0], menuItemCreationRequest);
+    }
+
+    getAllMenuItems(accessToken) {
+        return this._fetch(
+            'GET', `http://${this._inventoryServiceDomain}/org/menu/items`,
+            {'Authorization': `Bearer ${accessToken}`}, (json) => json.menuItems);
+    }
+
+    getMenuItem(accessToken, itemId) {
+        return this._fetch(
+            'GET', `http://${this._inventoryServiceDomain}/org/menu/items/${itemId}`,
+            {'Authorization': `Bearer ${accessToken}`}, (json) => json.menuItem);
+    }
+
+    updateMenuItemGeneral(accessToken, itemId, name, description, keywords, ingredients) {
+        const menuItemUpdateRequest = {
+            name: name,
+            description: description,
+            keywords: keywords,
+            ingredients: ingredients
+        };
+
+        return this._fetchWithData(
+            'PUT', `http://${this._inventoryServiceDomain}/org/menu/items/${itemId}`,
+            {'Authorization': `Bearer ${accessToken}`}, (json) => json.menuItem, menuItemUpdateRequest);
+    }
+
+    updateMenuItemImageSet(accessToken, itemId, imageSet) {
+        const menuItemUpdateRequest = {
+            imageSet: imageSet
+        };
+
+        return this._fetchWithData(
+            'PUT', `http://${this._inventoryServiceDomain}/org/menu/items/${itemId}`,
+            {'Authorization': `Bearer ${accessToken}`}, (json) => json.menuItem, menuItemUpdateRequest);
+    }
+
+    deleteMenuItem(accessToken, itemId) {
+        return this._fetch(
+            'DELETE', `http://${this._inventoryServiceDomain}/org/menu/items/${itemId}`,
+            {'Authorization': `Bearer ${accessToken}`}, (unused) => { return {}; });
+    }
+        
     getWebshopInfo(host) {
+        return this._fetch(
+            'GET', `http://${this._inventoryServiceDomain}/webshop`,
+            {'Host': host}, (json) => json.webshopInfo);
+    }
+
+    _fetchWithData(method, url, headers, extract, data) {
+        const options = {
+            method: method,
+            headers: headers,
+            body: JSON.stringify(data),
+            mode: 'cors',
+            cache: 'no-cache',
+            redirect: 'error',
+            referrer: 'client'
+        };
+
+        return this._commonFetch(url, options, extract);
+    }
+
+    _fetch(method, url, headers, extract) {
+        const options = {
+            method: method,
+            headers: headers,
+            mode: 'cors',
+            cache: 'no-cache',
+            redirect: 'error',
+            referrer: 'client'
+        };
+
+        return this._commonFetch(url, options, extract);
+    }
+
+    _commonFetch(url, options, extract) {
         return new Promise(
             (resolve, reject) => {
-                const options = {
-                    method: 'GET',
-                    headers: {
-                        'Host': host
-                    },
-                    mode: 'cors',
-                    cache: 'no-cache',
-                    redirect: 'error',
-                    referrer: 'client'
-                };
-
-                fetch(`http://${this._inventoryServiceDomain}/webshop`, options)
+                fetch(url, options)
                     .then((response) => {
-                       if (response.ok) {
-                           response.json()
-                               .then((json) => {
-                                   resolve(json.webshopInfo);
-                               })
-                               .catch((error) => {
-                                   reject(error);
-                               });
-                       } else {
-                           reject(response.status);
-                       }
-                   })
-                   .catch((error) => {
-                       reject(error);
-                   });
+                        if (response.ok) {
+                            response.json()
+                                .then((json) => {
+                                    resolve(extract(json));
+                                })
+                                .catch((error) => {
+                                    reject(error);
+                                });
+                        } else {
+                            reject(response.status);
+                        }
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
             }
         );
     }
